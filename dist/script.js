b2bb53249ba9d77b76018f1f5d54a94381c01c0e
cl3m3nt1n3 <shukyin.lau@outlook.com>
2021-03-22 16:11:02 +0000

    mobile nav

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,8 @@
-const menu_btn = document.querySelector('.hamburger');
+const menu_btn = document.querySelector('.header__top--menu');
+const navbar = document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('active');
+  navbar.classList.toggle('active');
 })
+
