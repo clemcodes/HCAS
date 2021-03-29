49a626d0c0e9c370d09b62bf626d9d4d6842ba1d
cl3m3nt1n3 <shukyin.lau@outlook.com>
2021-03-29 16:05:44 +0100

    carousel

--- dist/script.js
+++ dist/script.js
@@ -13,23 +13,23 @@ function plusSlides(n) {
   showSlides(slideIndex += n)
 }
 
-function currentSlides(n) {
-  showSlides(slideIndex = n)
-}
+// function currentSlides(n) {
+//   showSlides(slideIndex = n)
+// }
 
 function showSlides(n) {
   const slides = document.getElementsByClassName('carousel__slides');
-  const dots = document.getElementsByClassName("dot");
+  // const dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) slideIndex = slides.length
   for(let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"
   }
-  for(let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "")
-  }
+  // for(let i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "")
+  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active"
+  // dots[slideIndex-1].className += " active"
 }
 
 setInterval(function() {
