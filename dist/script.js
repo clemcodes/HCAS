f1db738c0d712ea665c068e40c0b60115e27bee0
cl3m3nt1n3 <shukyin.lau@outlook.com>
2021-03-29 13:25:07 +0100

    home mobile carousel

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,32 @@ menu_btn.addEventListener('click', function () {
   navbar.classList.toggle('active');
 })
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+function currentSlides(n) {
+  showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName('carousel__slides');
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) slideIndex = slides.length
+  for(let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  for(let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "")
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active"
+}
+
+setInterval(function() {
+  plusSlides(1);
+}, 4000)
